85383e850228a019f9939b8cc94056d5da123f49
YannickVeron <yveron74@gmail.com>
2020-04-19 18:15:05 +0200

    Add replyTo

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -14,6 +14,8 @@ use App\Entity\Comment;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
 use App\formulaire\FormComment;
 
 
@@ -28,7 +30,7 @@ class MovieController extends AbstractController
     {
         // request HTTP / API MovieDB
         $client = HttpClient::create();
-        $secret= "ef626857f1c8ea6ac2f82d74dd13de7d";
+        $secret= "key";
         $link = "https://api.themoviedb.org/3/discover/movie?api_key=".$secret;
         $response = $client->request('GET', $link);
         $statusCode = $response->getStatusCode();
@@ -43,6 +45,8 @@ class MovieController extends AbstractController
     public function show(int $id, EntityManagerInterface $entityManager, Request $request)
     {
         $ratingRepo = $entityManager->getRepository(Rating::class);
+        $commentRepo = $entityManager->getRepository(Comment::class);
+
         $queryAvgRating = $ratingRepo->createQueryBuilder('g')
             ->select("avg(g.value)")
             ->where('g.movieId = :idMovie')
@@ -58,6 +62,7 @@ class MovieController extends AbstractController
         $formComment = $this->createFormBuilder($comment)
             ->add('text', TextType::class)
             ->add('save', SubmitType::class)
+            ->add('replyToId',HiddenType::class,["mapped" => false])
             ->getForm();
 
         // Lors d'un envoie du formulaire on récupère les données
@@ -65,27 +70,30 @@ class MovieController extends AbstractController
         if ($formComment->isSubmitted() && $formComment->isValid()) {
             $comment->setUser($user);
             $comment->setMovieId($id);
+            if($request->request->get('form')['replyToId']!=""){
+                $comment->setReplyTo($commentRepo->find($request->request->get('form')['replyToId']));
+            }
             $entityManager->persist($comment);
             $entityManager->flush();
             return $this->redirectToRoute('movie_index');
         }
-
+        
+        
+        $comments = $commentRepo->findBy(['movieId' => $id,'replyTo'=>null]);
+        //dd($comments)
 
         // request HTTP / API MovieDB
         $client = HttpClient::create();
-        $secret= "ef626857f1c8ea6ac2f82d74dd13de7d";//to move elsewhere, .env maybe ?
+        $secret= "key";//to move elsewhere, .env maybe ?
         $link = "https://api.themoviedb.org/3/movie/".$id."?api_key=".$secret."&language=fr-FR";
         $response = $client->request('GET', $link);
         $content = $response->toArray();
-
-
 
         $links = "https://api.themoviedb.org/3/movie/".$id."/videos?api_key=".$secret."&language=fr-FR";
         $responses = $client->request('GET', $links);
         $trailer=  $responses->toArray();
         $c = $trailer['results'][0];
 
-
-        return $this->render("movie/show.html.twig",["movie"=>$content,"rating"=>$avgScore[array_key_first($avgScore)] , "trailer"=>$c , "formComment"=>  $formComment->createView()  ]);
+        return $this->render("movie/show.html.twig",["movie"=>$content,"rating"=>$avgScore[array_key_first($avgScore)] , "trailer"=>$c , "formComment"=>  $formComment->createView(),"comments"=>$comments]);
     }
 }
